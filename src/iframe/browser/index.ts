8e079978d27cf37f7ce0cddbfd55271f86ccb112
Dani Gámez Franco <gmzcodes@outlook.com>
2024-11-27 14:57:30 +0100

    Add mock for the Alarms API.

--- src/iframe/browser/index.ts
+++ src/iframe/browser/index.ts
@@ -1,13 +1,111 @@
 import enDic from "url:/assets/_locales/en/messages.json";
 import zhCnDic from "url:/assets/_locales/zh_CN/messages.json";
+import type { Alarms, Runtime } from "webextension-polyfill";
+
+// TODO: Missing storage.onChanged.addListener
+
+interface AlarmWithTimer extends Alarms.Alarm {
+  timeoutID?: number;
+  intervalID?: number;
+}
+
+const alarmsByName: Record<string, AlarmWithTimer> = {};
+
+type AlarmCallback = (alarm?: Alarms.Alarm) => void;
+
+const alarmCallbacks: AlarmCallback[] = [];
+
+function invokeAlarms(name: string) {
+  const alarmWithTimer = alarmsByName[name];
+
+  if (!alarmWithTimer) return;
+
+  alarmCallbacks.forEach((alarmCallback) => {
+    alarmCallback({
+      name: alarmWithTimer.name,
+      scheduledTime: alarmWithTimer.scheduledTime,
+      periodInMinutes: alarmWithTimer.periodInMinutes,
+    });
+  });
+}
 
 const alarms = {
-  create: null,
-  clear: null,
-  getAll: null,
+  create: (name: string, alarmInfo: Alarms.CreateAlarmInfoType) => {
+    const periodInMs = (alarmInfo.periodInMinutes ?? -1) * 60000;
+    const delayInMs = alarmInfo.when ? (
+      alarmInfo.when - Date.now()
+    ) : (
+      (alarmInfo.delayInMinutes ?? -1) * 60000
+    );
+
+    const alarmWithTimer: AlarmWithTimer = {
+      name,
+      scheduledTime: 0,
+      periodInMinutes: alarmInfo.periodInMinutes,
+    };
+
+    alarmsByName[name] = alarmWithTimer;
+
+    // TODO: Record last alarm run in localStorage to continue the alarm when reloading...
+
+    if (delayInMs === 0) {
+      alarmWithTimer.scheduledTime = Date.now();
+      invokeAlarms(name);
+    } else if (delayInMs > 0) {
+      alarmWithTimer.scheduledTime = Date.now() + delayInMs;
+
+      alarmWithTimer.timeoutID = setTimeout(() => {
+        delete alarmWithTimer.timeoutID;
+
+        invokeAlarms(name);
+
+        alarmWithTimer.scheduledTime = Date.now() + periodInMs;
+
+        alarmWithTimer.intervalID = setInterval(() => {
+          alarmWithTimer.scheduledTime = Date.now() + periodInMs;
+
+          invokeAlarms(name);
+        }, periodInMs);
+      }, delayInMs);
+    }
+
+    if (delayInMs <= 0 && periodInMs > 0) {
+      alarmWithTimer.scheduledTime = Date.now() + periodInMs;
+
+      alarmWithTimer.intervalID = setInterval(() => {
+        alarmWithTimer.scheduledTime = Date.now() + periodInMs;
+
+        invokeAlarms(name);
+      }, periodInMs);
+    }
+  },
+  clear: (name: string) => {
+    const alarmWithTimer = alarmsByName[name];
+
+    if (!alarmWithTimer) return;
+
+    if (alarmWithTimer.timeoutID) clearTimeout(alarmWithTimer.timeoutID);
+    if (alarmWithTimer.intervalID) clearTimeout(alarmWithTimer.intervalID);
+  },
+  getAll: () => {
+    return Promise.resolve(Object.values(alarmsByName) satisfies Alarms.Alarm[]);
+  },
+  get: (name: string) => {
+    const alarmWithTimer = alarmsByName[name];
+
+    if (!alarmWithTimer) return;
+
+    return {
+      name: alarmWithTimer.name,
+      scheduledTime: alarmWithTimer.scheduledTime,
+      periodInMinutes: alarmWithTimer.periodInMinutes,
+    };
+  },
   onAlarm: {
-    addEventListener: null
-  }
+    addListener: (alarmCallback: AlarmCallback) => {
+      alarmCallbacks.push(alarmCallback);
+    },
+  },
 };
 
 const dictionaries = {
@@ -55,7 +153,15 @@ const runtime = {
         default_popup: "popup.html"
       }
     };
-  }
+  },
+  onInstalled: {
+    addListener: (fn) => {
+      fn({ reason: "install", temporary: false } satisfies Runtime.OnInstalledDetailsType);
+    },
+  },
+
+  // TODO: onConnect is probably not needed once I replace onMessage/sendMessage with isomorphicOnMessage and
+  // isomorphicSendMessage (using the env variable).
 };
 
 const tabs = {
@@ -108,7 +214,7 @@ const tabs = {
 };
 
 export default {
-  // alarms,
+  alarms,
   i18n,
   runtime,
   tabs
